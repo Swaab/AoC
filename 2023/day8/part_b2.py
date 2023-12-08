ac36a8e44627d78cb44a8c29b7dd5a37fd080ded
Daniël Swaab <swaab.daniel@gmail.com>
2023-12-08 13:20:20 +0100

    cleeanup

--- 2023/day8/part_b2.py
+++ 2023/day8/part_b2.py
@@ -6,34 +6,23 @@ def run():
         instruction = lines[0]
         d = {}
         start_keys = []
-        end_keys = []
         for line in lines[2:]:
             node, l, r = [w.strip() for w in line.replace("= (",",").replace(")","").replace(" ","").split(",")]
             d[node] = [l, r]
             if "A" == node[2]:
                 start_keys.append(node)
-            elif "Z" == node[2]:
-                end_keys.append(node)
 
-        idx = 0
-        new_run = False
-        sum=1
-        for reps in start_keys:
-            results=[reps]
-            count=0
-            while not all([r[2]=="Z" for r in results]) or new_run:
-                new_run = False
+        l =[]
+        for result in start_keys:
+            idx = 0
+            while not result[2]=="Z":
                 i = idx % (len(instruction)-1)
                 action = instruction[i]
                 action_idx = 1 if action == 'R'else 0
-                for r_i, result in enumerate(results):
-                    results[r_i] = d[result][action_idx]
+                result = d[result][action_idx]
                 idx += 1
-                count+=1
-            print(count)
-            sum*=count
-            # print(idx)
-            new_run=True
-        print(sum)
-
+            l.append(idx)
+        print(f'use : "https://www.calculatorsoup.com/calculators/math/lcm.php" \n'
+          f'for the result numbers {l}')
+        
 run()
